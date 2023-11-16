ce2553fae6802a91503a2509dae2c00afb755c7b
dudkinma.24 <max_dudkin@list.ru>
2023-11-16 12:56:19 +0300

    ee

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -1,14 +1,43 @@
 package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@Controller
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ApiController {
-    @GetMapping("message")
-    public ResponseEntity<String> getText() {
-        return ResponseEntity.ok("Я Максим Дудкин сделал работу и получилось создать и запустить сервис");
+    private final List<String> messages = new ArrayList<>();
+    @GetMapping("messages")
+    public ResponseEntity<List<String>> getMessages() {
+        return ResponseEntity.ok(messages);
+    }
+    ///curl -X GET localhost:8080/messages
+    @PostMapping("messages")
+    public ResponseEntity<Void> addMessage(@RequestBody String text) {
+        messages.add(text);
+        return ResponseEntity.accepted().build();
+    }
+    ///curl -H "Content-Type: text/plain" -d gg localhost:8080/messages
+    @GetMapping("messages/{index}")
+    public ResponseEntity<String> getMessage(@PathVariable("index") Integer
+                                                     index) {
+        return ResponseEntity.ok(messages.get(index));
+    }
+    ///curl -X GET localhost:8080/messages/1
+    @DeleteMapping("messages/{index}")
+    public ResponseEntity<Void> deleteText(@PathVariable("index") Integer
+                                                   index) {
+        messages.remove((int) index);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("messages/{index}")
+    public ResponseEntity<Void> updateMessage(
+            @PathVariable("index") Integer i,
+            @RequestBody String message) {
+        messages.remove((int) i);
+        messages.add(i, message);
+        return ResponseEntity.accepted().build();
     }
 }
