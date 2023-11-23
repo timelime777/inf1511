de8113cb74b201830d61ab2a9c7bf3acfd19affd
dudkinma.24 <max_dudkin@list.ru>
2023-11-23 12:51:15 +0300

    ee2

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -32,6 +32,7 @@ public class ApiController {
         messages.remove((int) index);
         return ResponseEntity.noContent().build();
     }
+    ///curl -X DELETE localhost:8080/messages/0
     @PutMapping("messages/{index}")
     public ResponseEntity<Void> updateMessage(
             @PathVariable("index") Integer i,
@@ -40,4 +41,18 @@ public class ApiController {
         messages.add(i, message);
         return ResponseEntity.accepted().build();
     }
+    ///curl -X PUT -H "Content-Type: text/plain" -d PUT_SOMETHING localhost:8080/messages/1
+    @GetMapping("messages/search/{text}")
+    public ResponseEntity<Integer> searchMessage(
+            @PathVariable("text") String s){
+        int ans = -1;
+        for (String elem: messages){
+            if (elem.contains(s)){
+                ans = messages.indexOf(elem);
+                break;
+            }
+        }
+        return ResponseEntity.ok(messages.indexOf(messages.get(ans)));
+    }
+    //curl -X GET localhost:8080/messages/search/g
 }
